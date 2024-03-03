91fd52cda6f356d1f280da0b8330fdc6552304bd
Awais Tahseen <awaistahseenaccoun@gmail.com>
2024-03-03 11:59:57 +0500

    modified app.py  file

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import re
 from utils_r import pairs
 import streamlit as st
 
-def eliza(inp: str) -> str:
+def eliza(inp: str, show_regex: bool) -> str:
     for pair in pairs:
         pattern = re.compile(pair[0], re.IGNORECASE)
-        match = re.match(pattern, inp)
+        match = re.search(pattern, inp)
         if match:
             if match.groups(1):
                 return pair[1].format(match.group(1))
             else:
+                if show_regex:
+                    st.code(f"Regex Pattern: {pair[0]}", language='plaintext')
                 return pair[1]
     return "Sorry, I didn't understand your question"
 
@@ -23,9 +25,12 @@ def main():
     # Input
     user_input = st.text_input("You: ")
     
+    # Boolean option to show regex
+    show_regex = st.checkbox("Show Regex")
+    
     if st.button("Ask ELIZA"):
         # Process input and get result
-        result = eliza(user_input)
+        result = eliza(user_input, show_regex)
         
         # Display ELIZA's response
         st.write(result)
